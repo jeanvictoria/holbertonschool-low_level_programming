86d4a1b3f474dfd6254d2de991eae68fef6bbb3a
jeanvictoria <jean.vic8@gmail.com>
2019-06-27 19:21:47 +0000

    Solve point two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isdigit - fuction for evaluate if have digit.
+ * @c: variable to evaluate
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int r=a*b;
+	return (r);
+}
+

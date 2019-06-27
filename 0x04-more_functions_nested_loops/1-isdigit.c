6cb56906bb79293658eafa7b91292ef2327c4417
jeanvictoria <jean.vic8@gmail.com>
2019-06-27 18:02:40 +0000

    Solve point one and prototypes

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * isdigit - fuction for evaluate if have digit.
+ * @c: variable to evaluate
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 40 && c <= 57)
+	{
+	return (1);
+	}
+return (0);
+}

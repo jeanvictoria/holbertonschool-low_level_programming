7fb27f32b15b4657eab0bbf20e7a2cfcba342c4b
jeanvictoria <jean.vic8@gmail.com>
2019-06-27 21:28:06 +0000

    Solve point four

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - print numbers from zero to nine exept two and four
+ *
+*/
+void print_most_numbers(void)
+{
+	int number;
+
+		for (number = 0 ; number < 10 ; number++)
+			if (number != 2 && number !=4)
+			{
+			_putchar(number + '0');
+			}
+			_putchar(10);
+}
+

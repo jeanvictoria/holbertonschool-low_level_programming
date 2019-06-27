b93100f6c1f3cc56e8cac2b9f4d9a322adeb4faf
jeanvictoria <jean.vic8@gmail.com>
2019-06-27 21:34:03 +0000

    Solve point four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,7 @@ void print_most_numbers(void)
 	int number;
 
 		for (number = 0 ; number < 10 ; number++)
-			if (number != 2 && number !=4)
+			if (number != 2 && number != 4)
 			{
 			_putchar(number + '0');
 			}

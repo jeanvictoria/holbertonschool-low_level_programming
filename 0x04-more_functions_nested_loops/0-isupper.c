8794cacb04645234af5fd05f6c09ba49cc56b0a4
jeanvictoria <jean.vic8@gmail.com>
2019-06-27 17:01:28 +0000

    Solve point zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _isupper - Calculate supper
+ * @c: number to evaluate
+ *
+ * Return: 1 if is lower
+*/
+int _isupper(int c)
+{
+	return ((c >= 65 && c <= 90) ? 1 : 0);
+}

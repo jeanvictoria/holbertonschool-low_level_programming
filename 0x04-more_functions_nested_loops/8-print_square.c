10c01c5f51924b3fb1cdc505b6670b1d82b5361d
jeanvictoria <jeanvictoria>
2019-06-27 22:43:45 -0500

    Solve point eigth

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square - print # in matrix nxn
+ * @size: size of matrix
+ */
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
+		}
+	}
+	else
+		_putchar(10);
+}
+

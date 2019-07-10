9b456d6a66d79efbe70db5a7da348caf88a4fed6
jeanvictoria <jeanvictoria>
2019-07-09 20:44:48 -0500

    Solution point to eigth

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_diagsums - print sum of diag
+ *
+ * @a: array to be manipulater
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int diag1 = 0;
+	unsigned int diag2 = 0;
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				diag1 += *(a + (i * size) + j);
+			if ((i + j) == size - 1)
+				diag2 += *(a + (i * size) + j);
+		}
+	}
+	printf("%d, %d\n", diag1, diag2);
+}

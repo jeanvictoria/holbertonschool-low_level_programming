47525958f2ec41754e5ad9ac449f9fc5c6b33b5e
jeanvictoria <jeanvictoria>
2019-07-11 18:26:18 -0500

    Update

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,10 +1,13 @@
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
-int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
 int is_prime_number(int n);
+int number_is_prime(int base , int num);
+int _calculate_sqrt(int base, int num);
+int calculate_palindrome(char *s, int size, int current);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);

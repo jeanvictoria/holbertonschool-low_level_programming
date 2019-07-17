332d00593397f0c7aad97a2827ba982ff196f67d
jeanvictoria <jeanvictoria>
2019-07-16 23:52:58 -0500

    advance

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtow - create grid
+ * @str: grid
+ * Return: array
+ */
+char **strtow(char *str);
+{
+	char **p;
+
+	return (p);
+}

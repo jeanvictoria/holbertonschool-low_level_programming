4495ecbfea977e9b3a61d450544d923e55bfacbc
jeanvictoria <jeanvictoria>
2019-07-18 21:12:55 -0500

    Solution point to zero

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * malloc_checked - use exit
+ * @b: num of bytes
+ * Return: pointer that allocate memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}

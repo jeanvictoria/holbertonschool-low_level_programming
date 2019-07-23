9ed08b51bd08530f2d1c8582c4da525291bf946c
jeanvictoria <jeanvictoria>
2019-07-22 21:41:25 -0500

    Solution to point one

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize structure dog
+ * @d: Dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner  to initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

f9efbf742b02ed1e39284bd3a66c10b35d3b95ed
jeanvictoria <jeanvictoria>
2019-07-22 21:50:45 -0500

    Solution to point two

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - Print simple dog
+ * @d: pointer to struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		(d->name == NULL) ? printf("%p\n", d->name) : printf("%s\n", d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: ");
+		(d->owner == NULL) ? printf("%p\n", d->owner) : printf("%s\n", d->owner);
+	}
+}

79499f8f24a6ebba849f84d71ffead08ecff8a9f
jeanvictoria <jeanvictoria>
2019-08-01 22:52:07 -0500

    Solution to point two

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - add node to list
+ * @head: head of the list
+ * @str: value
+ * Return: list_list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	int size = 0;
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	for (; new_node->str[size]; size++)
+		;
+	new_node->str[size] = '\0';
+	new_node->len = size;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}

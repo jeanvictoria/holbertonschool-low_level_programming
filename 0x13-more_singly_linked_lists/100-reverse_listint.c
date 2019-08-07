c15b362cadb5f85beb7010a6a3b42a56a2fa8d22
jeanvictoria <jeanvictoria>
2019-08-06 23:51:52 -0500

    Files

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse list
+ * @head: lists head
+ * Return: head of list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	if ((*head) == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		listint_t *aux;
+
+		aux = *head;
+		*head = (*head)->next;
+		aux->next = reverse_listint(head);
+		*head = aux;
+		return (*head);
+	}
+

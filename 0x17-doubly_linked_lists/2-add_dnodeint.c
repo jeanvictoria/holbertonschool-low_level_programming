3a9f46d00516eb132dba9d6b70e5c9a63116aa9c
jeanvictoria <jeanvictoria>
2019-09-15 23:52:20 -0500

    Solution to point two

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to end
+ * @head: head of the list
+ * @n: number
+ * Return: address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_element;
+
+	new_element = malloc(sizeof(dlistint_t));
+	if (!new_element)
+	{
+		return (NULL);
+	}
+	new_element->prev = NULL;
+	new_element->next = NULL;
+	if (head != NULL)
+		new_element->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_element;
+	new_element->n = n;
+	*head = new_element;
+	return (new_element);
+}

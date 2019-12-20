3406c8179a4e8f85537de645703d919c3b337554
jeanvictoria <jean.vic8@gmail.com>
2019-12-20 09:38:39 -0500

    All project

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_binary - print number in binary
+ * @n: number to print
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		printf("0");
+	else
+	{
+		unsigned long int aux;
+
+		aux = n >> 1;
+		if (aux)
+			print_binary(aux);
+		_putchar((n & 1) + '0');
+	}
+}
